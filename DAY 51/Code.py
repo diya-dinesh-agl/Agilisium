7c59d1a0cf9255c601a193817747e0ec093a215f
diya-dinesh-agl <diya.dinesh@agilisium.com>
2025-07-01 16:05:09 +0530

    Update Code.py

--- DAY 51/Code.py
+++ DAY 51/Code.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        res = []
+        n = 1
+        for val in target:
+            while n<val:
+                res+=["Push","Pop"]
+                n+=1
+            res.append("Push")
+            n+=1
+        return res
