c4a861b76fcc431ed82016b6161a53b3cd730bc8
diya-dinesh-agl <diya.dinesh@agilisium.com>
2025-07-07 17:09:08 +0530

    Update Code.py

--- DAY 57/Code.py
+++ DAY 57/Code.py
@@ -1,1 +1,8 @@
-
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs: return ""
+        for i,c in enumerate(zip(*strs)):
+            if len(set(c))>1:
+                return strs[0][:i]
+        return min(strs,key=len)
+        
