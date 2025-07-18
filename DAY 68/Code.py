28ec7a7af90150582de4b5bcee0ed6fda0c9317d
diya-dinesh-agl <diya.dinesh@agilisium.com>
2025-07-18 12:44:09 +0530

    Update Code.py

--- DAY 68/Code.py
+++ DAY 68/Code.py
@@ -1,4 +1,6 @@
-def maxRotateFunction(self, A):
-    f = r = sum(i*x for i,x in enumerate(A)); s = sum(A)
-    for x in reversed(A): f += s - len(A)*x; r = max(r,f)
-    return r
+class Solution(object):
+    def maxRotateFunction(self, A):
+        f=r= sum(i*x for i,x in enumerate(A));s=sum(A)
+        for x in reversed(A): f+=s-len(A)*x; r =max(r,f)
+        return r
+        
